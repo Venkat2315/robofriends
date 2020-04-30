b09516af161f22954202efaf21d65938ba0a151b
TENNETI VENKAT <tennetihemant@gmail.com>
2020-04-30 17:44:51 +0530

    Add files via upload

---
+++ build/precache-manifest.0f0c4e849428018cdfb778a033ad72cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f213678339383ea7b11e04a6718800",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "8236e54f8eb4689359cf",
+    "url": "/static/css/main.3fe003d4.chunk.css"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8236e54f8eb4689359cf",
+    "url": "/static/js/main.1a30139f.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  }
+]);
